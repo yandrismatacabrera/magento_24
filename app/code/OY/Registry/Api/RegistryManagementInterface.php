b4992cb9926397c815535f2f622ef58dfe8db567
Osvaldo Stable <osvaldo.stable@onetree.com>
2020-12-03 00:39:23 -0300

    module registry

--- app/code/OY/Registry/Api/RegistryManagementInterface.php
+++ app/code/OY/Registry/Api/RegistryManagementInterface.php
@@ -20,4 +20,10 @@ interface RegistryManagementInterface
      * @return string
      */
     public function createCustomer();
+
+    /**
+     * POST for Registry api
+     * @return string
+     */
+    public function registryCustomer();
 }
