f83eb19f695d06f4b3b3bd016671985e77043d9f
Denis <dlespinosa365@gmail.com>
2020-12-20 10:52:16 -0300

    se incluye vuejs y se mejora interfaz y tratamiendo de errores

--- app/code/Gym/JsAuth/view/adminhtml/requirejs-config.js
+++ app/code/Gym/JsAuth/view/adminhtml/requirejs-config.js
@@ -1,6 +1,7 @@
 var config = {
         map: {
             '*': {
+                'vue': 'Gym_JsAuth/js/vue',
                 'faceApi': 'Gym_JsAuth/js/face-api.min',
                 'jsAuth': 'Gym_JsAuth/js/jsAuth'
             }
--- app/code/Gym/JsAuth/view/adminhtml/web/js/jsAuth.js
+++ app/code/Gym/JsAuth/view/adminhtml/web/js/jsAuth.js
@@ -1,154 +1,232 @@
 define([
     'faceApi',
     'jquery',
-    'underscore',
+    'vue',
     'domReady!'
 ], function jsAuth(
     faceApi,
     jQuery,
-    _
+    Vue
 ) {
 
     'use strict';
 
-    window.faceApp = {
-        running: false,
-        faceDetected: false,
-        base64Image: null,
-        config: {}
-    }
-    var video = document.getElementById('main-video');
-
-    var Utils = {
-        makeCallToApi: function makeCallToApi() {
-            var settings = {
-                "async": true,
-                "crossDomain": true,
-                "dataType": "json",
-                "url": window.faceApp.config.luxandApiUrl,
-                "method": "POST",
-                "headers": {
-                    "token": window.faceApp.config.luxandToken
-                },
-                "data": {"photo": window.faceApp.base64Image }
-            }
-            jQuery('#person-image').attr('src', 'data:image/png;base64,' + window.faceApp.base64Image)
-            jQuery.ajax(settings)
-                .done(function(response) {
-                    Utils.handleResponseApi(response);
-                })
-                .error(function (error) {
-                    Utils.showError();
-                })
-                .then(function () {
-                    window.faceApp.faceDetected = false;
-                    jQuery('#video-header-card').text('Video');
-                });
-        },
-
-        showPerson: function showPerson(person) {
-            try {
-                var data = JSON.parse(person.name);
-                jQuery('#person-name').html(data.name);
-                jQuery('#person-ci').html(data.ci);
-                jQuery('#person-email').html(data.email);
-            } catch (e) {
-                console.log(e)
-            }
-            return null;
-        },
-
-        handleResponseApi: function handleResponseApi(response) {
-            var person;
-            var personArray;
-            if (Array.isArray(response)) {
-                personArray = response.filter(person => person.probability > 0.9);
-                if (personArray) {
-                    person = personArray && personArray[0];
-                    if (person) {
-                        return Utils.showPerson(person);
-                    }
-                }
-            }
-            jQuery('#video-header-card').text('Hubo un problema con el reconocimiento...');
-        },
-        showError: function showError(error) {
-            console.error(error);
-        },
-
-        getImageFromVideo: function getImageFromVideo() {
-            const canvas = document.createElement('canvas');
-            canvas.width = video.videoWidth;
-            canvas.height = video.videoHeight;
-            canvas.getContext('2d')
-                .drawImage(video, 0, 0, canvas.width, canvas.height);
-            return canvas.toDataURL().replace('data:image/png;base64,', '');
-        },
-
-        initListeners: function initListeners() {
-            jQuery('[data-action="toggleRecognition"]').click(_.bind(Utils.toggleRecognition, Utils));
-        },
-
-        toggleRecognition: function toggleRecognition() {
-            if (!window.faceApp.running) {
-                window.faceApp.running = true;
-                this.initFaceDetection()
-            }
-        },
-
-        initFaceDetection: async function initFaceDetection() {
-            const canvas = faceApi.createCanvasFromMedia(video);
-            const displaySize = { width: video.videoWidth, height: video.videoHeight };
-            var detections = null;
-            var resizedDetections;
-
-            faceApi.matchDimensions(canvas, displaySize);
-            while (!window.faceApp.faceDetected) {
-                jQuery('#video-header-card').text('Detectando...');
-                detections = await faceApi.detectSingleFace(video, new faceApi.TinyFaceDetectorOptions());
-                if (detections) {
-                    resizedDetections = faceApi.resizeResults(detections, displaySize);
-                    if (detections.score > 0.8) {
-                        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
-                        faceApi.draw.drawDetections(canvas, resizedDetections);
-
-                        window.faceApp.faceDetected = true;
-                        window.faceApp.running = false;
-                        window.faceApp.base64Image = Utils.getImageFromVideo();
-                        jQuery('#video-header-card').text('Identificando...');
-                        Utils.makeCallToApi();
-                    }
-                }
-                setTimeout(function () {
-                }, 100);
-            }
-        },
-
-        initVideo: function initVideo(video) {
-            navigator.getUserMedia(
-                { video: {} },
-                function (stream) {
-                    video.srcObject = stream;
-                    Utils.initListeners();
-                },
-                err => console.error(err)
-            );
-        },
-
-        initApp: function initApp(config) {
-            return Promise.all([
-                faceApi.nets.tinyFaceDetector.loadFromUri(config.assetBaseUrl + '/models'),
-                faceApi.nets.faceLandmark68Net.loadFromUri(config.assetBaseUrl + '/models'),
-                faceApi.nets.faceRecognitionNet.loadFromUri(config.assetBaseUrl + '/models')
-            ]).then(function () {
-                Utils.initVideo(video);
-            });
-        }
-    };
 
     return function (config) {
-        window.faceApp.config = config;
-        Utils.initApp(config);
+        const vueApp = new Vue({
+            el: '#appvue',
+            mounted: function mounted() {
+                const self = this;
+                this.video = document.getElementById('main-video');
+                this.isLoadinLibraries = true;
+                return Promise.all([
+                    faceApi.nets.tinyFaceDetector.loadFromUri(self.config.assetBaseUrl + '/models'),
+                    faceApi.nets.faceLandmark68Net.loadFromUri(self.config.assetBaseUrl + '/models'),
+                    faceApi.nets.faceRecognitionNet.loadFromUri(self.config.assetBaseUrl + '/models')
+                ]).then(function () {
+                    self.isLoadinLibraries = false;
+                    self.initVideo();
+                });
+            },
+            data: {
+                isLoadinLibraries: false,
+                config: config,
+                video: null,
+                isDetecting: false,
+                isIdentifying: false,
+                isRegistering: false,
+                faceDetected: null,
+                identifiedPerson: null,
+                isSucessfullyRegister: null,
+                info: { msg: 'Bienvenido', type: 'primary' }
+            },
+            computed: {
+                isProcessing: function  isProcessing () {
+                    return this.isLoadinLibraries || 
+                            this.isDetecting ||
+                            this.isIdentifying ||
+                            this.isRegistering;
+                },
+                isSucessfullyIdentify: function isSucessfullyIdentify() {
+                    return !this.isProcessing && 
+                            this.identifiedPerson;
+                }
+            },
+            watch: {
+                isDetecting: function faceDetected(newValue) {
+                   this.info = Object.assign({}, {
+                        msg: 'Detectando...', 
+                        type: 'warning' ,
+                        showSpinner: true
+                   }) 
+                },
+                isIdentifying: function isIdentifying() {
+                    this.info = Object.assign({}, {
+                        msg: 'Identificando...', 
+                        type: 'warning',
+                        showSpinner: true
+                    }) 
+                },
+                isRegistering: function isRegistering() {
+                    this.info = Object.assign({}, {
+                        msg: 'Registrando el acceso...', 
+                        type: 'warning',
+                        showSpinner: true
+                    });
+                },
+                isSucessfullyIdentify: function isSucessfullyIdentify() {
+                    if (this.isSucessfullyIdentify) {
+                        this.info = Object.assign({}, {
+                            msg: 'Bienvenido ' + this.identifiedPerson.name , 
+                            type: 'success' 
+                        });
+                    }
+                },
+                isSucessfullyRegister: function isSucessfullyRegister(newValue) {
+                    if (newValue === false) {
+                        this.info = Object.assign({}, {
+                            msg: 'Error registrando el usuario intente nuevamente.', 
+                            type: 'danger' 
+                        });
+                    }
+                    if (newValue === true) {
+                        this.info = Object.assign({}, {
+                            msg: 'Registro de acceso exitoso.', 
+                            type: 'success' 
+                        });
+                    }
+                }
+                
+            },
+            
+            methods: {
+                resetValues: function resetValues() {
+                    this.isDetecting = false;
+                    this.isIdentifying = false;
+                    this.isRegistering = false;
+                    this.faceDetected = null;
+                    this.identifiedPerson = null;
+                    this.info = { msg: 'Bienvenido', type: 'primary' };
+                    return null;
+                },
+
+                initFaceDetection: async function initFaceDetection() {
+                    const canvas = faceApi.createCanvasFromMedia(this.video);
+                    const displaySize = { 
+                        width: this.video.videoWidth, 
+                        height: this.video.videoHeight 
+                    };
+                    const self = this;
+                    var detections = null;
+                    var resizedDetections;
+                    
+
+                    this.isDetecting = true;
+                    this.isIdentifying = false;
+                    this.isRegistering = false;
+                    this.faceDetected = null;
+                    this.identifiedPerson = null;
+        
+                    faceApi.matchDimensions(canvas, displaySize);
+                    while (!self.faceDetected) {
+                        
+                        detections = await faceApi.detectSingleFace(this.video, new faceApi.TinyFaceDetectorOptions());
+                        
+                        if (detections) {
+                            resizedDetections = faceApi.resizeResults(detections, displaySize);
+                            if (detections.score > 0.9) {
+                                canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+                                faceApi.draw.drawDetections(canvas, resizedDetections);
+                                self.faceDetected = self.getImageFromVideo();
+                                self.isDetecting = false;
+                                self.isIdentifying = true;
+                                self.checkFaceWithLuxand();
+                            }
+                        }
+                        setTimeout(function () {
+                        }, 100);
+                    }
+                },
+                initVideo: function initVideo() {
+                    var self = this; 
+                    return navigator.getUserMedia(
+                        { video: {} },
+                        function (stream) {
+                            self.video.srcObject = stream;
+                        },
+                        err => console.error(err)
+                    );
+                },
+                getImageFromVideo: function getImageFromVideo() {
+                    const canvas = document.createElement('canvas');
+                    canvas.width = this.video.videoWidth;
+                    canvas.height = this.video.videoHeight;
+                    canvas.getContext('2d')
+                        .drawImage(this.video, 0, 0, canvas.width, canvas.height);
+                    return canvas.toDataURL().replace('data:image/png;base64,', '');
+                },
+                checkFaceWithLuxand: function checkFaceWithLuxand() {
+                    const self = this;
+                    var settings = {
+                        "async": true,
+                        "crossDomain": true,
+                        "dataType": "json",
+                        "url": this.config.luxandApiUrl,
+                        "method": "POST",
+                        "headers": {
+                            "token": this.config.luxandToken
+                        },
+                        "data": {"photo": this.faceDetected }
+                    }
+                    return jQuery.ajax(settings)
+                        .done(function(response) {
+                            self.identifiedPerson = self.handleResponseApi(response);
+                            self.isIdentifying = false;
+                        })
+                        .always(function() {
+                            self.isIdentifying = false;
+                        });
+                },
+                handleResponseApi: function handleResponseApi(response) {
+                    var personArray;
+                    var person;
+                    try {
+                        if (Array.isArray(response)) {
+                            personArray = response.filter(person => person.probability > 0.9);
+                            if (personArray) {
+                                person = personArray && personArray[0] && JSON.parse(personArray[0].name);
+                                person.image64 = this.faceDetected;
+                                return person;
+                            }
+                        }
+                    } catch (e) {
+                        console.log(e);
+                    }
+                    return null;
+                },
+                accessRegister: function accessRegister() {
+                    const settings = {
+                        "async": true,
+                        "dataType": "json",
+                        "url": this.config.registerUrl,
+                        "method": "POST",
+                        "data": { "customer_id": this.identifiedPerson.id }
+                    }
+                    const self = this;
+                    this.isRegistering = true;
+                    return jQuery.ajax(settings)
+                        .done(function (response) {
+                            self.isSucessfullyRegister = response && response.success;
+                        })
+                        .fail(function () {
+                            self.isSucessfullyRegister = false;
+                        })
+                        .always(function() {
+                            self.isRegistering = false;
+                        })
+                }
+            }
+         });
     };
 
 })
